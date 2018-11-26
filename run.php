3cb375f38fa05becaf26897ca3358d6f39e9acff
taisa <g5.taisa831@gmail.com>
2018-11-26 11:34:26 +0900

    fix

--- run.php
+++ run.php
@@ -2,4 +2,4 @@
 require_once dirname(__FILE__) . '/src/AmazonPhotoFormatter.php';
 
 $amazonPhotoRenamer = new \amazonphotoformatter\AmazonPhotoFormatter(dirname(__FILE__));
-$amazonPhotoRenamer->run();
+$amazonPhotoRenamer->format();
